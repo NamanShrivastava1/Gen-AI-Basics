0cc268d05029f592e4232cb7c66f197fdecdb03b
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-04-03 14:43:43 +0530

    LangChain Tools, Integrated Email Sending using tools

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ import "dotenv/config";
 import readline from "readline/promises";
 // readline is a built-in module in Node.js for reading input from the Terminal.
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage } from "langchain";
+import { HumanMessage, tool, createAgent } from "langchain";
+import { sendEmail } from "./mail.service.js";
+import * as z from "zod";
+// Zod used for correct data shape when using tools, it validates the input data against the defined schema and provides type safety.
+
+const emailTool = tool(sendEmail, {
+  name: "emailTool",
+  description:
+    "Send an email when the user explicitly asks. Call this tool only once and then return a final confirmation.",
+  schema: z.object({
+    to: z.string(),
+    subject: z.string(),
+    html: z.string(),
+  }),
+});
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -13,18 +27,27 @@ const model = new ChatMistralAI({
   model: "mistral-small-latest",
 });
 
-const messages = []
+const agent = createAgent({
+  model,
+  tools: [emailTool],
+});
+
+const messages = [];
 
 while (true) {
   const userInput = await rl.question("\x1b[32mYou: \x1b[0m");
 
   messages.push(new HumanMessage(userInput));
 
-  const response = await model.invoke(messages);
+  const response = await agent.invoke({
+    messages,
+  });
 
-  messages.push(response);
+  messages.push(response.messages[response.messages.length - 1]);
+  console.log(response);
+  // console.log(response.messages[response.messages.length - 1].text);
 
-  console.log(`\x1b[34mMistralAI:\x1b[0m ${response.content}`);
+  // console.log(`\x1b[34mMistralAI:\x1b[0m ${response.content}`);
 }
 
 rl.close();
